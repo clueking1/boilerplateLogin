16b8319ffe76f4afbe8a2222d52388f976315d97
clueking1 <wcoan300@gmail.com>
2020-03-15 13:12:22 -0400

    added server express code

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const session = require('express-session')
+const passport = require('./config/passport')
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
+
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true}))
+//app.use(passport.initialize())
+//app.use(passport.session())
+
+app.listen(PORT, () => {
+    console.log('http://localhost:' + PORT)
+})
