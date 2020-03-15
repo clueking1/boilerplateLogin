fe1484d9b78ebe720c99e67c258f0f507aeb5afc
clueking1 <wcoan300@gmail.com>
2020-03-15 15:04:28 -0400

    made sure usernames and emails are unique and do not exsist

--- models/user.js
+++ models/user.js
@@ -5,8 +5,33 @@ const con = require('../config/config')
 
 const bcrypt = require('bcryptjs')
 
+// const create = {
+//     create: function(username, email, password) {
+//         password = bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+        
+//         con.query('INSERT INTO user (username, email, password) VALUES (?, ?, ?)', 
+//         [username, email, password], (err, data) => {
+//             if (err) {
+//                 throw err
+//             }
+//             console.log('logged!!!')
+//             return data
+//         })
+           
+//     }
+// }
+
 const create = {
     create: function(username, email, password) {
+        con.query("SELECT COUNT(*) AS cnt FROM user WHERE email = ? AND username = ?" , 
+        [email, username] , function(err , data){
+   if(err){
+       console.log(err);
+   }   
+   else{
+       if(data[0].cnt > 0){  
+             console.log('already exsists') 
+       }else{
         password = bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
         
         con.query('INSERT INTO user (username, email, password) VALUES (?, ?, ?)', 
@@ -15,9 +40,11 @@ const create = {
                 throw err
             }
             console.log('logged!!!')
-            return data
-        })
-           
+            //return data
+        })               
+       }
+   }
+})
     }
 }
 
