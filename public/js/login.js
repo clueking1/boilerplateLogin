7bd4894a2a8b69415e65efbe11ca3bc1dc986c83
clueking1 <wcoan300@gmail.com>
2020-03-16 13:57:07 -0400

    added a login in form to test login... successfulgit add .

---
+++ public/js/login.js
@@ -0,0 +1,39 @@
+const loginForm = document.querySelector('.login')
+const emailInput = document.querySelector('#email-input')
+const passwordInput = document.querySelector('#password-input')
+
+loginForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+    const userData = {
+        email: emailInput.value,
+        password: passwordInput.value
+    }
+    if(!userData.email || !userData.password) {
+        return
+    }
+    loginUser(userData.email, userData.password)
+     emailInput.value = ""
+    passwordInput.value = ""
+})
+
+function loginUser(username, password) {
+     fetch('/api/login', {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
+    }).then(() => {
+        window.location.replace('/members')
+    }).catch(err => {
+        if (err) {
+            throw err
+        }
+    })
+}
+
+
