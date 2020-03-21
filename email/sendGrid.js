e3e275872569a8573c0ed8293414b2a41d7b313e
clueking1 <wcoan300@gmail.com>
2020-03-21 10:00:27 -0400

    need to figure out URL to plug in confirmation code

--- email/sendGrid.js
+++ email/sendGrid.js
@@ -4,17 +4,17 @@ const sendGrid = require('sendgrid').mail;
 console.log(process.env.SENDGRID_API_KEY)
 
 function sendEmail (to, token) {
-  const hostUrl = process.env.hostURL
+  //const hostUrl = process.env.hostURL
     const sgMail = require('@sendgrid/mail');
-    sgMail.setApiKey('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');
+    sgMail.setApiKey('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');
 const msg = {
   to: to,
   from: 'test@example.com',
-  subject: 'Sending with Twilio SendGrid is Fun',
-  text: `Click on this link to verify your email ${hostUrl}/verification?token=${token}&email=${to}`,
-  html: `<strong>Click on this link to verify your email ${hostUrl}/verification?token=${token}&email=${to}</strong>`
+  subject: 'Account Confirmation',
+  text: `Click on this link to verify your email /verification?token=${token}&email=${to}`,
+  html: `<strong>Click on this link to verify your email /verification?token=${token}&email=${to}</strong>`
 };
-console.log(hostUrl)
+//console.log(hostUrl)
 console.log(msg)
 sgMail.send(msg);
     
